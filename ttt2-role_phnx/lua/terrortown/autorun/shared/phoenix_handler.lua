a910b8ab2d34eef25ef51a181828fd8202a89e53
milkwxter <ethan.r.bates2003@gmail.com>
2025-02-08 10:41:12 -0600

    Finished role

--- ttt2-role_phnx/lua/terrortown/autorun/shared/phoenix_handler.lua
+++ ttt2-role_phnx/lua/terrortown/autorun/shared/phoenix_handler.lua
@@ -1,10 +1,9 @@
 -- my variables
-local isPhoenixReviving = false
 local phoenixBodies = {}
 
 -- my hooks
 if SERVER then
-	hook.Add( "TTTOnCorpseCreated", "TTT2PhoenixBodyCreated", function(rag, ply)
+	hook.Add("TTTOnCorpseCreated", "TTT2PhoenixBodyCreated", function(rag, ply)
 		-- make sure its a real corpse
 		if not CORPSE.IsRealPlayerCorpse(rag) then
 			print("[REALLY BAD] Entity was not a real player corpse.")
@@ -21,10 +20,26 @@ if SERVER then
 	end )
 	
 	hook.Add("Think", "TTT2CheckPhoenixBodies", function()
+		-- iterate through all our phoenix bodies
         for _, rag in ipairs(phoenixBodies) do
+			-- store the current player of the phoenix body
+			local ply = CORPSE.GetPlayer(rag)
+			-- early return statements
+			if ply:IsReviving() then return end
+			-- if body is on fire, we revive
             if rag:IsOnFire() then
-                print("Ahhh! Phoenix on fire!")
+				print("Revival should start.")
+                ply:Revive(3, function(p)
+					p:SetHealth(50)
+					p:GiveEquipmentItem("item_ttt_nofiredmg")
+					print("Revival done.")
+					table.remove(phoenixBodies, _)
+				end, nil, false, REVIVAL_BLOCK_NONE)
             end
         end
     end)
+	
+	hook.Add("TTTEndRound", "TTT2CleanupPhoenix", function()
+		phoenixBodies = {}
+	end)
 end
